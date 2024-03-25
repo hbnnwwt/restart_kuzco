6d138de92b5974fc930fd54b02cdf56b6caeda01
hbnnwwt <116838445+hbnnwwt@users.noreply.github.com>
2024-03-25 23:50:50 +0800

    Update check_kuzco.sh

--- check_kuzco.sh
+++ check_kuzco.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+workers=29#根据自己gpu能力，更改此数值
 if pgrep -x "kuzco-runtime" > /dev/null
 then
    top -b -n 1|grep --count 'kuzco'
@@ -5,5 +7,5 @@ then
 else
     echo "kuzco-runtime is not running. Killing all kuzco processes..."
     killall kuzco
-    for i in {1..29}; do (nohup kuzco worker start > /dev/null 2>&1 & sleep 3); done
+    for ((i=0;i<workers;i++)); do (nohup kuzco worker start > /dev/null 2>&1 & sleep 3); done
 fi
